c8b8c4c5f39493f02a77a5138bd136d94f5c1539
Martin Ben <mebenav88@gmail.com>
2016-02-01 17:30:31 -0500

    add conditional statement to world cup single page, if year is found

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,43 @@ var worldcupsJson = require('../worlcups.json');
 exports.home = function(req,res){
   // res.send('this is the server response on the home page');
 
-  var worldcups = worldcupsJson.worldcups
+  var worldcups = worldcupsJson.worldcups;
   res.render('home',{
-    title: 'Worlcup page',
+    title: 'FIFA Worldcups',
     worldcups: worldcups
   });
 };
 
 //worldcup_single
 exports.worldcup_single = function(req, res){
+  var worldcups = worldcupsJson.worldcups;
+
   var worldcup_number = req.params.worldcup_year;
-  res.send("this is the page for worlcup " + worldcup_number);
+
+  // var worlcups_arr = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970,
+  //                     1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006,
+  //                     2010, 2014
+  // ]
+
+  var worlcups_arr = [1970, 1974, 1978]
+
+  for(var i = 0; i < worlcups_arr.length; i++){
+    if(worlcups_arr[i] == worldcup_number){
+      var worldcup = worldcups[i];
+      var title = worldcup.title;
+
+      res.render('worldcup_single.ejs',{
+        worldcups: worldcups,
+        title: title,
+        worldcup: worldcup
+      });
+      break;
+    }else{
+      res.send("this is not the page you are looking!");
+      break;
+    }
+  }
+
 };
 
 //not found page
