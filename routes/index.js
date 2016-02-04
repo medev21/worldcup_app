d30c94eb8d9b6176277cf3714d2ccf1e85d356c9
Martin Ben <mebenav88@gmail.com>
2016-02-04 17:51:46 -0500

    add for loop to access each worlcup in json file, and its properties

--- routes/index.js
+++ routes/index.js
@@ -18,29 +18,55 @@ exports.worldcup_single = function(req, res){
 
   var worldcup_number = req.params.worldcup_year;
 
+  // console.log(worldcups[1].year);
+
+  // var worldcups_arr = ["1970", "1974", "1978"]
+
+  for(var i = 0; i < worldcups.length; i++){
+    if(worldcups[i].year == worldcup_number){
+      var worldcup = worldcups[i];
+      var title = worldcups[i].title;
+      var year = worldcups[i].year;
+    }
+  }
+
+  if(year == worldcup_number){
+    res.render('worldcup_single.ejs',{
+      worldcups: worldcups,
+      title: title,
+      worldcup: worldcup
+    });
+  }else{
+    res.send("this is not the page you are looking!");
+
+  }
+
+
+
+
   // var worlcups_arr = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970,
   //                     1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006,
   //                     2010, 2014
   // ]
 
-  var worlcups_arr = [1970, 1974, 1978]
-
-  for(var i = 0; i < worlcups_arr.length; i++){
-    if(worlcups_arr[i] == worldcup_number){
-      var worldcup = worldcups[i];
-      var title = worldcup.title;
-
-      res.render('worldcup_single.ejs',{
-        worldcups: worldcups,
-        title: title,
-        worldcup: worldcup
-      });
-      break;
-    }else{
-      res.send("this is not the page you are looking!");
-      break;
-    }
-  }
+  // var worldcups_arr = [ "1974", "1978"]
+  //
+  // for(var i = 0; i < worldcups_arr.length; i++){
+  //   if(worldcups_arr[i] == worldcup_number){
+  //     var worldcup = worldcups[i];
+  //     var title = worldcup.title;
+  //
+  //     res.render('worldcup_single.ejs',{
+  //       worldcups: worldcups,
+  //       title: title,
+  //       worldcup: worldcup
+  //     });
+  //     break;
+  //   }else{
+  //     res.send("this is not the page you are looking!");
+  //     break;
+  //   }
+  // }
 
 };
 
