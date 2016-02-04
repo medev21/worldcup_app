fba25e39cfb20d1c48eb58deaa57294b72cbaa33
Martin Ben <mebenav88@gmail.com>
2016-02-04 17:52:14 -0500

    remove old code

--- routes/index.js
+++ routes/index.js
@@ -41,33 +41,6 @@ exports.worldcup_single = function(req, res){
 
   }
 
-
-
-
-  // var worlcups_arr = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970,
-  //                     1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006,
-  //                     2010, 2014
-  // ]
-
-  // var worldcups_arr = [ "1974", "1978"]
-  //
-  // for(var i = 0; i < worldcups_arr.length; i++){
-  //   if(worldcups_arr[i] == worldcup_number){
-  //     var worldcup = worldcups[i];
-  //     var title = worldcup.title;
-  //
-  //     res.render('worldcup_single.ejs',{
-  //       worldcups: worldcups,
-  //       title: title,
-  //       worldcup: worldcup
-  //     });
-  //     break;
-  //   }else{
-  //     res.send("this is not the page you are looking!");
-  //     break;
-  //   }
-  // }
-
 };
 
 //not found page
